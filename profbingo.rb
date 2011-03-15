e99cd188139fcf373f1cd569e1994a34f34aee32
Eric Stokes <fernferret@gmail.com>
2011-03-15 17:05:31 -0400

    Sync Repositories

--- profbingo.rb
+++ profbingo.rb
@@ -1,13 +1,5 @@
-# Template from https://github.com/sinatra/heroku-sinatra-app
-#
-# You'll need to require these if you
-# want to develop while running with ruby.
-# The config/rackup.ru requires these as well
-# for it's own reasons.
-#
-# $ ruby heroku-sinatra-app.rb
-#
 require 'rubygems'
+require 'bundler/setup'
 require 'sinatra'
 
 configure :production do
@@ -23,13 +15,3 @@ get '/' do
   "Woot!
   ProfBingo is running on Heroku!"
 end
-
-# Test at <appname>.heroku.com
-
-# You can see all your app specific information this way.
-# IMPORTANT! This is a very bad thing to do for a production
-# application with sensitive information
-
-# get '/env' do
-#   ENV.inspect
-# end
